bb1293dae24333ecf9d38ebef00411f539d0a7de
Udey Rishi <udey.rishi@gmail.com>
2016-02-21 03:26:08 -0700

    Added random failure to lib_insertion_sort.c

--- DataSorter/src/lib_insertion_sort.c
+++ DataSorter/src/lib_insertion_sort.c
@@ -1,16 +1,55 @@
-#include <jni.h>
 #include "NativeInsertionSortOperation.h"
-#include <stdio.h>
+#include <jni.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
 
 void insertion_sort(jint *data, int length, double failure_probability);
+bool check_failure(uint64_t count, double failure_probability);
+int get(jint *data, int index, double failure_probability);
+void swap(jint *data, int i, int j);
+void fail();
 
 JNIEXPORT void JNICALL Java_NativeInsertionSortOperation_insertionSort
   (JNIEnv *env, jobject object, jintArray data, jdouble failure_probability) {
 
     jsize length = (*env)->GetArrayLength(env, data);
     jint *data_c = (*env)->GetIntArrayElements(env, data, 0);
+    srand(time(NULL));
     insertion_sort(data_c, length, failure_probability);
     (*env)->ReleaseIntArrayElements(env, data, data_c, 0);
+}
+
+void insertion_sort(jint *data, int length, double failure_probability) {
+    for (int end = length; end >= 0; --end) {
+        for (int i = 0; i < end - 1; ++i) {
+
+            int index = i;
+            for (int j = index + 1;
+                 j > 0 && (get(data, j, failure_probability) < get(data, index, failure_probability));
+                 --j) {
+                swap(data, j, index--);
+            }
+
+        }
+    }
+}
+
+bool check_failure(uint64_t count, double failure_probability) {
+    double hazard = count * failure_probability;
+    double random_event = (double)rand()/(double)RAND_MAX;
+    return random_event >= 0.5 && random_event <= 0.5 + hazard;
+}
+
+int get(jint *data, int index, double failure_probability) {
+    static uint64_t count = 0;
+
+    if (check_failure(++count, failure_probability)) {
+        fail();
+    }
+
+    return data[index];
 }
 
 void swap(jint *data, int i, int j) {
@@ -19,13 +58,6 @@ void swap(jint *data, int i, int j) {
     data[i] = temp;
 }
 
-void insertion_sort(jint *data, int length, double failure_probability) {
-    for (int end = length; end >= 0; --end) {
-        for (int i = 0; i < end - 1; ++i) {
-            int index = i;
-            for (int j = index + 1; j > 0 && (data[j] < data[index]); --j) {
-                swap(data, j, index--);
-            }
-        }
-    }
+void fail() {
+    exit(-1);
 }
