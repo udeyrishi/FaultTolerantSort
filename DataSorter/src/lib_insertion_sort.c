b0e0604ccdcc5c8a7da7b6fd4b9d181f5a9a78ff
Udey Rishi <udey.rishi@gmail.com>
2016-02-21 02:02:51 -0700

    Added template for the native insertion sort calls

---
+++ DataSorter/src/lib_insertion_sort.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include "NativeInsertionSortLib.h"
+#include <stdio.h>
+
+void insertion_sort(jint *data, int length, double failure_probability);
+
+JNIEXPORT void JNICALL Java_NativeInsertionSortLib_insertion_1sort
+  (JNIEnv *env, jobject object, jintArray data, jdouble failure_probability) {
+
+    jsize length = (*env)->GetArrayLength(env, data);
+    jint *data_c = (*env)->GetIntArrayElements(env, data, 0);
+    insertion_sort(data_c, length, failure_probability);
+    (*env)->ReleaseIntArrayElements(env, data, data_c, 0);
+}
+
+void insertion_sort(jint *data, int length, double failure_probability) {
+    
+}
